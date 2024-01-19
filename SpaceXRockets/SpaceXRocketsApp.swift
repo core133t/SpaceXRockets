5584ef3dd398cb992b471027ac918d74fcd0892d
Логинов Дмитрий <dmitriy.v.loginov@gazprombank.ru>
2024-01-19 03:15:16 +0300

    Initial Commit

---
+++ SpaceXRockets/SpaceXRocketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXRocketsApp.swift
+//  SpaceXRockets
+//
+//  Created by Dmitry Loginov on 19.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXRocketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
